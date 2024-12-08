4cc2d2140702f4d2b17d245ae90412b2ca652c16
stambaughkarla <stambaughkarla@gmail.com>
2024-12-08 16:53:46 -0600

    saves for review

--- FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ FinalProject/obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc358ca8e99aa0302e9012021168147ec40f71e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16dc8e5087e66c89086b5cf83bbd43e2dbec4289")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
